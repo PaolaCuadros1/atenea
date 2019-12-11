e43f8606151038a6e9de4cb2f7ac2899be81ce70
Cuadros1605 <jeissonrosas@wssinternational.net>
2019-12-11 12:03:46 -0500

    Creacion del proyecto atenea.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+//Acá deben agregar las rutas.
+const appRoutes: Routes = [
+  //{path: 'main', component: MainComponent}, -> Ruta de ejemplo.
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
