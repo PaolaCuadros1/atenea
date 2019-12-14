055ee4d760be83e4794c01ca7241d6890d9c4df7
Cuadros1605 <jeissonrosas@wssinternational.net>
2019-12-14 18:25:26 -0500

    Valide que todos los campos del formulario tuvieran datos almacenados.

---
+++ src/js/atenea.js
@@ -0,0 +1,18 @@
+function validateEmptyData() {
+
+  if ($('#nombre').val() == '' || $('#apellido').val() == '' || $('#correo').val() == '' || $('#contrasena').val() == '') {
+    $('.error').show();
+    $('.msg').text(' Debes llenar todos los campos!');
+  } else {
+    //
+    console.log($('input[name=generos]:checked').length)
+    if ($('input[name=generos]:checked').length > 0) {
+      $('#saveData').attr('type', 'submit');
+      $('.error').hide();
+    } else {
+      $('.error').show();
+      $('.msg').text(' Selecciona un genero favorito, por favor!');
+    }
+  }
+
+}
