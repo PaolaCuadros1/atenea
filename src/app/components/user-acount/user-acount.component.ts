a050cd9626486ba948413ba35d8491e54bebe737
aspcruz <aspcjack@gmail.com>
2019-12-16 17:39:05 -0500

    nuevos estilos

--- src/app/components/user-acount/user-acount.component.ts
+++ src/app/components/user-acount/user-acount.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import * as M from '../../../assets/materialize/js/materialize.js';
+
 
 @Component({
   selector: 'app-user-acount',
   templateUrl: './user-acount.component.html',
   styleUrls: ['./user-acount.component.css']
 })
-export class UserAcountComponent implements OnInit {
-  options = {fullWidth: true, indicators: true};
 
+export class UserAcountComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit() {
-    var elems = document.querySelectorAll('.carousel');
-    var instance = M.Carousel.init(elems, this.options);
-  }
+}
 }
