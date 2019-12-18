6d2c4d5a6a1aac084769b95759ed0be8efae3523
carito893 <carito893@gmail.com>
2019-12-18 12:38:28 -0500

    creacion componente saludo usuario

---
+++ src/app/components/saludo/saludo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from "../../model/usuario";
+import { UserService } from '../../services/usuario.service';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+
+@Component({
+  selector: 'app-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+  public url: String;
+  public identidad;
+  constructor(
+    private usuarioService: UserService,
+    private _router: Router) {
+    this.url=usuarioService.url;
+  }
+
+  ngOnInit() {
+    this.identidad = this.usuarioService.obtenerNombreUsuario();
+    console.log("-----> Identidad",  this.identidad)
+  }
+
+}
