6435a9b5c2247171c6b05a37be96a577fd988fe7
JeimmyAU <jaguilaru@correo.udistrital.edu.co>
2019-12-18 12:26:52 -0500

    quedo funcionando el boton de iniciar sesion como administrador. Cuando entra, ahora aparece el menu del administrador.

--- src/js/atenea.js
+++ src/js/atenea.js
@@ -1,43 +1,48 @@
 function validateEmptyData() {
 
-  if ($('#nombre').val() == '' || $('#apellido').val() == '' || $('#correo').val() == '' || $('#contrasena').val() == '') {
-    $('.error').show();
-    $('.msg').text(' Debes llenar todos los campos!');
-  } else {
-    //
-    console.log($('input[name=generos]:checked').length)
-    if ($('input[name=generos]:checked').length > 0) {
-      $('#saveData').attr('type', 'submit');
-      $('.error').hide();
+    if ($('#nombre').val() == '' || $('#apellido').val() == '' || $('#correo').val() == '' || $('#contrasena').val() == '') {
+        $('.error').show();
+        $('.msg').text(' Debes llenar todos los campos!');
     } else {
-      $('.error').show();
-      $('.msg').text(' Selecciona un genero favorito, por favor!');
+        //
+        console.log($('input[name=generos]:checked').length)
+        if ($('input[name=generos]:checked').length > 0) {
+            $('#saveData').attr('type', 'submit');
+            $('.error').hide();
+        } else {
+            $('.error').show();
+            $('.msg').text(' Selecciona un genero favorito, por favor!');
+        }
     }
-  }
 
 }
 
-function logged(){
-  $('.notLoggedIn').hide();
-  $('.logged').show();
-   $('.redes').hide();
-  //  $('.redes2').show();
+function logged() {
+    $('.notLoggedIn').hide();
+    $('.logged').show();
+    $('.redes').hide();
+    //  $('.redes2').show();
 }
 
-function cerrarSesion(){
-  $('.notLoggedIn').show();
-  $('.logged').hide();
-  localStorage.removeItem('sesion');
-  localStorage.clear();
-   $('.redes').show();
+function cerrarSesion() {
+    $('.notLoggedIn').show();
+    $('.logged').hide();
+    localStorage.removeItem('sesion');
+    localStorage.clear();
+    $('.redes').show();
 }
 
 function play(e) {
-  e.preventDefault();
-  document.getElementById('sipnosis-movie').style.display = 'none';
+    e.preventDefault();
+    document.getElementById('sipnosis-movie').style.display = 'none';
 
-  const video = document.getElementById('movie');
-  if (video && video.src) {
-    video.src += '?autoplay=1';
-  }
+    const video = document.getElementById('movie');
+    if (video && video.src) {
+        video.src += '?autoplay=1';
+    }
+}
+
+function printMenuAdmin() {
+    $('.adminLoged').show();
+    $('.notred').hide();
 }
